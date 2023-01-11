6a9854f15e3b7281375e5cb9d660bfd44f67fe22
butters5789 <butters5789@gmail.com>
2023-01-11 13:22:52 -0700

    my first props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,11 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard(props) {
+  return (
+    <div>
+      Profile Card
+      <div>Title is {props.title}</div>
+      <div>Handle is {props.handle}</div>
+    </div>
+  );
 }
 
 export default ProfileCard;
