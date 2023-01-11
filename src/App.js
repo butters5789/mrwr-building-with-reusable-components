f8c66438f0d60de4c348b0d92a29cc2c5df9e551
butters5789 <butters5789@gmail.com>
2023-01-11 14:49:58 -0700

    Styling using Bulma an external css library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
-
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
@@ -7,11 +7,44 @@ import SiriImage from './images/siri.png';
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was made by Amazon."
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft."
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,22 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <img src={image} alt={`${title} logo`} />
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt={`${title} logo`} />
+        </figure>
+      </div>
 
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
+        </div>
+
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
